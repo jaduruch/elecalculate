5a4c88fd2a457a59d02388af9376eaabd243683c
Paul1404 <Paul1404@users.noreply.github.com>
2025-11-03 22:26:55 +0000

    deploy: 0d01da6bccbbad0b3fd57ab697d526bbf1d78ca8

---
+++ angular/src/main.ts
@@ -0,0 +1,7 @@
+import './style.css';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+
+bootstrapApplication(AppComponent).catch((err) =>
+  console.error(err)
+);
